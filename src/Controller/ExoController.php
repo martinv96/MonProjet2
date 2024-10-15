ba27734242a1a1c42d813f0e2b1a9fb747ae73ba
martin <martinv9663@gmail.com>
2024-10-15 12:30:18 +0200

    ajout route inscription et connexion avec conditions vérifs tests

--- src/Controller/ExoController.php
+++ src/Controller/ExoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -43,7 +44,9 @@ HTML;
         return new Response("Salut $nom", Response::HTTP_OK);
     }
 
-    
+    // dans une fonction/méthode, on peut déclarer une variable dans les parametres directement en placant request devant la variable.
+    // ex : function test(Request $a) {}
+
     #[Route("/calcul/{a}/{b}", name: "addition", methods: ["GET"])]
     // ajout d'une boucle if pour renvoyer une réponse dans le cas ou l'utilisateur ne tape pas un chiffre.
     function addition($a,$b) {
@@ -53,4 +56,56 @@ HTML;
         $resultat = $a + $b;
         return new Response("<h1>page /addition </h1> <br>résultat de $a et $b :  $resultat");
     }
+
+    #[Route("/inscription", name: "inscription", methods:["POST"])]
+
+    function insription(Request $requete){
+        // var_dump($requete->request);
+        return new Response(
+            $requete->request->get('email'),
+            Response::HTTP_OK
+        );
+    }
+
+
+    // Créer une Route /connexion avec la méthode POST
+    // Injecter l'objet requête dans l'action.
+    // Récupérer le nom, prénom, email et mot de passe du corps de la requête
+    // Retourner une réponse de succès.
+    // Tester la route avec Postman ou Thunder
+
+    // bonus
+    // tester si les données sont correctes, et retourner un message d'erreur si pas correcte
+    // tester l'appli avec un regex
+    // le nom et le prenom ne doivent pas etre vides
+    // le mot de passe doit etre supérieur à 6 caractères
+
+    #[Route("/connexion", name: "connexion", methods:["POST"])]
+
+    function connexion(Request $request) {
+        $nom = $request->request->get('nom');
+        $prenom = $request->request->get('prenom');
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        if (empty($nom) || empty($prenom)) {
+            return new Response("Le nom et le prénom ne doivent pas être vides.", Response::HTTP_BAD_REQUEST);
+        }
+
+        if (!preg_match('/^[A-Za-zÀ-ÖØ-öø-ÿ\s-]+$/', $nom) || !preg_match('/^[A-Za-zÀ-ÖØ-öø-ÿ\s-]+$/', $prenom)) {
+            return new Response("Le nom et le prénom doivent contenir uniquement des lettres.", Response::HTTP_BAD_REQUEST);
+        }
+
+        if (strlen($password) <= 6) {
+            return new Response("Le mot de passe doit contenir plus de 6 caractères.", Response::HTTP_BAD_REQUEST);
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return new Response("Adresse email invalide.", Response::HTTP_BAD_REQUEST);
+        }
+
+        // Si toutes les validations passent, retourner une réponse de succès
+        return new Response("Connexion réussie pour $prenom $nom", Response::HTTP_OK);
+    }
+
 }
