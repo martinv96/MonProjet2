414cedfb4e7dd0de569f40f5f5d290ee47f2b3c5
martin <martinv9663@gmail.com>
2024-10-24 12:18:59 +0200

    modif css

--- src/Controller/TachesController.php
+++ src/Controller/TachesController.php
@@ -14,55 +14,48 @@ class TachesController extends AbstractController
 {
     #[Route('/todolist/{id}/taches', name: 'taches.create', methods: ['POST'])]
     public function creerTache(Request $request, TodosList $todosList, TachesRepository $repository): Response
-{
-    $titre = $request->request->get('title');
-    $isFinished = $request->request->get('isFinished') !== null;
+    {
+        $titre = $request->request->get('title');
+        $isFinished = $request->request->get('isFinished') !== null;
 
-    if (empty($titre)) {
-        return $this->json(['erreur' => 'Le titre est obligatoire'], 400);
+        if (empty($titre)) {
+            return $this->json(['erreur' => 'Le titre est obligatoire'], 400);
+        }
+
+        $nouvelleTache = new Taches();
+        $nouvelleTache->setTitle($titre)
+            ->setIsFinished($isFinished)
+            ->setTodosList($todosList)
+            ->setCreatedAt(new \DateTime());
+
+        $repository->save($nouvelleTache, true);
+
+        return $this->redirectToRoute('todos_list.show', ['id' => $todosList->getId()]);
     }
 
-    $nouvelleTache = new Taches();
-    $nouvelleTache->setTitle($titre)
-                  ->setIsFinished($isFinished)
-                  ->setTodosList($todosList)
-                  ->setCreatedAt(new \DateTime());
+    #[Route('/taches/{id}/delete', name: 'taches.delete', methods: ['POST', 'DELETE'])]
+    public function deleteTache(Taches $taches, TachesRepository $repository): Response
+    {
+        $todosList = $taches->getTodosList();
+        $repository->supprimer($taches, true);
 
-    $repository->save($nouvelleTache, true);
-
-    return $this->redirectToRoute('todos_list.show', ['id' => $todosList->getId()]);
-}
-
-#[Route('/taches/{id}/delete', name: 'taches.delete', methods: ['POST','DELETE'])]
-public function deleteTache(Taches $taches, TachesRepository $repository): Response
-{
-    $todosList = $taches->getTodosList();
-    $repository->supprimer($taches, true); 
-
-    return $this->redirectToRoute('todos_list.show', ['id' => $todosList->getId()]);
-}
-
-#[Route('/taches/{id}/update', name: 'taches.update', methods: ['POST'])]
-public function updateTache($id, TachesRepository $repository, Request $req): Response
-{
-    // Récupérer la tâche
-    $tache = $repository->find($id);
-    if (!$tache) {
-        throw $this->createNotFoundException("Tâche non trouvée");
+        return $this->redirectToRoute('todos_list.show', ['id' => $todosList->getId()]);
     }
 
-    // Vérifier si la case à cocher 'isFinished' est cochée
-    $isFinished = $req->request->get('isFinished') !== null;
+    #[Route('/taches/{id}/update', name: 'taches.update', methods: ['POST'])]
+    public function updateTache($id, TachesRepository $repository, Request $req): Response
+    {
+        $tache = $repository->find($id);
+        if (!$tache) {
+            throw $this->createNotFoundException("Tâche non trouvée");
+        }
 
-    // Mettre à jour le statut de la tâche
-    $tache->setIsFinished($isFinished);
+        $isFinished = $req->request->get('isFinished') !== null;
 
-    // Sauvegarder les modifications
-    $repository->save($tache, true);
+        $tache->setIsFinished($isFinished);
 
-    // Rediriger ou retourner une réponse appropriée
-    return $this->redirectToRoute('todos_list.show', ['id' => $tache->getTodosList()->getId()]);
-}
+        $repository->save($tache, true);
 
-
+        return $this->redirectToRoute('todos_list.show', ['id' => $tache->getTodosList()->getId()]);
+    }
 }
